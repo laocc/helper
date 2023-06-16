a80d26cc5621be5ff2ab05f1adf43dd6a4b86197
老船长 <fazo@qq.com>
2023-06-16 14:24:20 +0800

    error

--- function/judge.php
+++ function/judge.php
@@ -267,7 +267,7 @@ function is_card(string $code): bool
     if (empty($code)) return false;
     if (!preg_match('/^\d{6}(\d{8})\d{3}(\d|x)$/i', $code, $mac)) return false;
     $num = str_split($code, 2);
-    if ($num[1] === '00' or $num[2] === '00') return false;
+    if ($num[2] === '00') return false;//县区代码不能为00
     $pro = [11, 12, 13, 14, 15, 21, 22, 23, 31, 32, 33, 34, 35, 36, 37, 38, 41, 42, 43, 44, 45, 46, 50, 51, 52, 53, 54, 61, 62, 63, 64, 65, 71, 81, 82];
     if (!in_array($num[0], $pro)) return false;
     if (!is_date($mac[1])) return false;
